bb9dda4ef993937c9ec3e6ecda0dbfa788b29416
ASTRO063 <manikanta.kodimala3@gmail.com>
2021-06-03 11:08:18 +0530

    subtraction-feature is added

---
+++ calculator.py
@@ -0,0 +1,8 @@
+def subtraction(num1,num2):
+    return num1-num2
+
+if __name__=="__main__":
+    print("enter two numbers")
+    num1=int(input())
+    num2=int(input())
+    print("difference of two numbers is",subtraction(num1,num2))
